d9527b717a3230a08195bb5e604e1944c5b98d64
MichaelSNelson <85200392+MichaelSNelson@users.noreply.github.com>
2024-02-28 15:06:13 -0600

    Update groovy scripts

--- src/main/groovyScripts/MikeLaptopSettings.groovy
+++ src/main/groovyScripts/MikeLaptopSettings.groovy
@@ -4,21 +4,21 @@ guiscript=true
 // Instrument: CAMM
 
 preferenceSettings = getQuPath().getPreferencePane().getPropertySheet().getItems()
-preferenceSettings.find{it.getName() == "PycroManager Script Path"}.setValue("C:\\ImageAnalysis\\QPExtension0.5.0\\qp-scope\\src\\main\\pythonScripts/4x_bf_scan_pycromanager.py")
-preferenceSettings.find{it.getName() == "Python Environment Path"}.setValue("C:\\Anaconda\\envs\\paquo")
-preferenceSettings.find{it.getName() == "Projects Folder Path"}.setValue("C:\\ImageAnalysis\\QPExtension0.5.0\\data\\slides")
-preferenceSettings.find{it.getName() == "Extension Path"}.setValue("C:\\ImageAnalysis\\QPExtension0.5.0\\qp-scope")
+preferenceSettings.find{it.getName() == "PycroManager Path"}.setValue("C:\\ImageAnalysis\\QPExtension0.5.0\\qp-scope\\src\\main\\pythonScripts/4x_bf_scan_pycromanager.py")
+preferenceSettings.find{it.getName() == "Python Environment"}.setValue("C:\\Anaconda\\envs\\paquo")
+preferenceSettings.find{it.getName() == "Projects Folder"}.setValue("C:\\ImageAnalysis\\QPExtension0.5.0\\data\\slides")
+preferenceSettings.find{it.getName() == "Extension Location"}.setValue("C:\\ImageAnalysis\\QPExtension0.5.0\\qp-scope")
 preferenceSettings.find{it.getName() == "Tissue Detection Script"}.setValue("DetectTissue.groovy")
 preferenceSettings.find{it.getName() == "First Scan Type"}.setValue("4x_bf")
 preferenceSettings.find{it.getName() == "Second Scan Type"}.setValue("20x_bf")
 preferenceSettings.find{it.getName() == "Tile Handling Method"}.setValue("None") // "Zip" or "Delete" are functional, anything else does nothing
-preferenceSettings.find{it.getName() == "Pixel Size for First Scan Type"}.setValue(1.105)
-preferenceSettings.find{it.getName() == "Pixel Size Source"}.setValue(7.2)
-preferenceSettings.find{it.getName() == "Pixel Size for Second Scan Type"}.setValue(0.5)
-preferenceSettings.find{it.getName() == "Camera Frame Width in Pixels"}.setValue(1392)
-preferenceSettings.find{it.getName() == "Camera Frame Height in Pixels"}.setValue(1040)
+preferenceSettings.find{it.getName() == "1st scan pixel size um"}.setValue(1.105)
+preferenceSettings.find{it.getName() == "Macro image px size"}.setValue(7.2)
+preferenceSettings.find{it.getName() == "2nd scan pixel size um"}.setValue(0.5)
+preferenceSettings.find{it.getName() == "Camera Frame Width #px"}.setValue(1392)
+preferenceSettings.find{it.getName() == "Camera Frame Height #px"}.setValue(1040)
 preferenceSettings.find{it.getName() == "Tile Overlap Percent"}.setValue(0)
 preferenceSettings.find{it.getName() == "Compression type"}.setValue(OMEPyramidWriter.CompressionType.J2K_LOSSY)
-
+print("If no errors, update completed successfully.")
 
 import qupath.lib.images.writers.ome.OMEPyramidWriter
