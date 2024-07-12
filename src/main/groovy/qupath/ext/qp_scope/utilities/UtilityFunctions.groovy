4af1afd73af72fa5c31f126a1bd5f2b7b8b0a819
MichaelSNelson <85200392+MichaelSNelson@users.noreply.github.com>
2024-07-12 13:17:17 -0500

    Update UtilityFunctions.groovy

--- src/main/groovy/qupath/ext/qp_scope/utilities/UtilityFunctions.groovy
+++ src/main/groovy/qupath/ext/qp_scope/utilities/UtilityFunctions.groovy
@@ -162,7 +162,7 @@ class UtilityFunctions {
             Process process = Runtime.getRuntime().exec(command);
             //Only show progress bars for collections
             if (progressBar && totalTifFiles != 0) {
-                UI_functions.showProgressBar(tifCount, totalTifFiles, process, 10000)
+                UI_functions.showProgressBar(tifCount, totalTifFiles, process, 20000)
                 //UI_functions.showProgressBar(tifCount, totalTifFiles)
             }
 
